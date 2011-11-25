26ffe13a3b26cc2d455e2449ba2f20873fcb462a
Fredrick Galoso <fred@wayoutmind.com>
2011-11-25 16:24:16 -0600

    add dymanic navigation

--- pjax_menu.js
+++ pjax_menu.js
@@ -1,7 +1,8 @@
 (function($){
 	$(document).ready(function() {
-		$('.menu a').pjax('#main')
-		    .live('click', function() {
+		$('.menu a').pjax('#main').click(function() {
+		   $('.current_page_item').removeClass('current_page_item');
+		   $(this).parent().addClass('current_page_item');
 		});
 	});
 })(jQuery);
