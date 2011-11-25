1601ab27c57c04f50373fe5890d098efc1029e6b
Fredrick Galoso <fred@wayoutmind.com>
2011-11-25 16:06:04 -0600

    refactor

--- pjax.php
+++ pjax.php
@@ -7,59 +7,59 @@
 define('THEMATICPJAX_URL', get_bloginfo('stylesheet_directory'));
 
 class ThematicPJAX {
-	public static function load() {
-		wp_enqueue_script('pjax', THEMATICPJAX_URL . '/pjax/jquery.pjax.js', array('jquery'));
-		wp_enqueue_script('pjax_menu', THEMATICPJAX_URL . '/pjax_menu.js', array('jquery', 'pjax'));
-	}
+    public static function load() {
+        wp_enqueue_script('pjax', THEMATICPJAX_URL . '/pjax/jquery.pjax.js', array('jquery'));
+        wp_enqueue_script('pjax_menu', THEMATICPJAX_URL . '/pjax_menu.js', array('jquery', 'pjax'));
+    }
 
+    /**
+     * PJAX render templates
+     */
 
-  /**
-   * PJAX render templates
-   */
-  # Default template
-	public static function Page() {
-		if (array_key_exists('HTTP_X_PJAX', $_SERVER) && $_SERVER['HTTP_X_PJAX']) {
-      include('templates/page.php');
-      exit;
-		}
-	}
+    # Default template
+    public static function Page() {
+        if (array_key_exists('HTTP_X_PJAX', $_SERVER) && $_SERVER['HTTP_X_PJAX']) {
+            include('templates/page.php');
+            exit;
+        }
+    }
 
-  # Full width content template
-	public static function FullWidth() {
-		if (array_key_exists('HTTP_X_PJAX', $_SERVER) && $_SERVER['HTTP_X_PJAX']) {
-		  
-		  # Example: Conditional template based on post's template (defined via Wordpress Administration)
-		  # global $wp_query;
-      # $template_name = get_post_meta( $wp_query->post->ID, '_wp_page_template', true );
+    # Full width content template
+    public static function FullWidth() {
+        if (array_key_exists('HTTP_X_PJAX', $_SERVER) && $_SERVER['HTTP_X_PJAX']) {
+          
+          # Example: Conditional template based on post's template (defined via Wordpress Administration)
+          # global $wp_query;
+          # $template_name = get_post_meta( $wp_query->post->ID, '_wp_page_template', true );
       
-      include('templates/template-page-fullwidth.php');
-      exit;
+          include('templates/template-page-fullwidth.php');
+          exit;
       
-		}
-	}
-	
-	# Blog content template
-	public static function Blog() {
-		if (array_key_exists('HTTP_X_PJAX', $_SERVER) && $_SERVER['HTTP_X_PJAX']) {
-      include('templates/template-page-blog.php');
-      exit;
-		}
-	}
-	
-	# Archives template
-	public static function Archives() {
-		if (array_key_exists('HTTP_X_PJAX', $_SERVER) && $_SERVER['HTTP_X_PJAX']) {
-      include('templates/archives.php');
-      exit;
-		}
-	}
-	
-	# Links template
-	public static function Links() {
-		if (array_key_exists('HTTP_X_PJAX', $_SERVER) && $_SERVER['HTTP_X_PJAX']) {
-      include('templates/links.php');
-      exit;
-		}
-	}
+        }
+    }
+    
+    # Blog content template
+    public static function Blog() {
+        if (array_key_exists('HTTP_X_PJAX', $_SERVER) && $_SERVER['HTTP_X_PJAX']) {
+            include('templates/template-page-blog.php');
+            exit;
+        }
+    }
+    
+    # Archives template
+    public static function Archives() {
+        if (array_key_exists('HTTP_X_PJAX', $_SERVER) && $_SERVER['HTTP_X_PJAX']) {
+            include('templates/archives.php');
+            exit;
+        }
+    }
+    
+    # Links template
+    public static function Links() {
+        if (array_key_exists('HTTP_X_PJAX', $_SERVER) && $_SERVER['HTTP_X_PJAX']) {
+            include('templates/links.php');
+            exit;
+        }
+    }
 }
 ?>
