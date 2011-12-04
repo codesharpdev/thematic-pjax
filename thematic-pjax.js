debf02efcf739281954caffa2c52c828a261986c
Fredrick Galoso <fred@wayoutmind.com>
2011-12-04 14:51:10 -0600

    added fully dynamic template types

--- thematic-pjax.js
+++ thematic-pjax.js
@@ -1,15 +1,22 @@
 (function($){
-	$(document).ready(function() {
+    $(document).ready(function() {
 
-	    // Load PJAX on navigation interaction
-		$('.menu a').pjax('#main').click(function() {
-		   $('.current_page_item').removeClass('current_page_item');
-		   $(this).parent().addClass('current_page_item');
-		});
+        // Load PJAX on navigation interaction
+        $('.menu a').pjax('#main').click(function() {
+           $('.current_page_item').removeClass('current_page_item');
+           $(this).parent().addClass('current_page_item');
+        });
 
-		// Watch PJAX requests so meta data in <head> can be modified accordingly
-		$(document).ajaxSuccess(function(event, request, settings) {
-		    console.log(request.getAllResponseHeaders());
-		});
-	});
+        // Watch PJAX requests so meta data in page can be modified accordingly
+        $('body').bind('pjax:start', function() {
+            $(this).ajaxSuccess(function(event, request, settings) {
+                // Modify CSS template
+                var classes = $(this).attr('class');
+                var template = request.getResponseHeader('X-PJAX-Template');
+                if (classes.indexOf(template) === -1) {
+                    $(this).attr('class', classes.replace(/page-template-([^\s]+)/, template));
+                }
+            });
+        });
+    });
 })(jQuery);
