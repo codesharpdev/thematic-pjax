ef73dd60b1ddb24c31cca8f72609088a193aeb0c
Fredrick Galoso <fred@wayoutmind.com>
2011-12-16 14:37:26 -0600

    Modify metadata on PJAX load

--- templates/meta.php
+++ templates/meta.php
@@ -1,10 +1,21 @@
 <?php
+/** 
+ * Supply metadata to thematic-pjax.js for client-side modification
+ */
+
 header('X-Link-Canonical: ' . get_permalink());
+
+$previous_id = get_adjacent_post($in_same_cat = false, $excluded_categories = '', $previous = true)->ID;
+$next_id = get_adjacent_post($in_same_cat = false, $excluded_categories = '', $previous = false)->ID;
+
 if (!is_singular()) {
     header('X-Link-Previous: ' . get_previous_posts_page_link());
     header('X-Link-Next: ' . get_next_posts_page_link());
 } else {
-    header('X-Link-Previous: ' . get_permalink(get_adjacent_post($in_same_cat = false, $excluded_categories = '', $previous = true)->ID));
-    header('X-Link-Next: ' . get_permalink(get_adjacent_post($in_same_cat = false, $excluded_categories = '', $previous = false)->ID)); 
+    header('X-Link-Previous: ' . get_permalink($previous_id));
+    header('X-Link-Next: ' . get_permalink($next_id));
 }
+
+header('X-Title-Previous: ' . get_the_title($previous_id));
+header('X-Title-Next: ' . get_the_title($next_id));
 ?>
--- thematic-pjax.js
+++ thematic-pjax.js
@@ -1,13 +1,13 @@
 (function($){
     $(document).ready(function() {
-
-        // Load PJAX on navigation interaction
+        /** Load PJAX on navigation interaction
+          */
         $('.menu a').pjax('#main').click(function() {
            $('.current_page_item').removeClass('current_page_item');
            $(this).parent().addClass('current_page_item');
         });
-
-        // Watch PJAX requests so meta data in document can be modified accordingly
+        /** Watch PJAX requests so metadata in document can be modified accordingly
+          */
         $('body').bind('pjax:start', function() {
             $(this).ajaxSuccess(function(event, request, settings) {
                 // CSS template
@@ -16,11 +16,29 @@
                 if (classes.indexOf(template) === -1) {
                     $(this).attr('class', classes.replace(/page-template-([^\s]+)/, template));
                 }
+                var link = {
+                  canonical: request.getResponseHeader('X-Link-Canonical'),
+                  previous: request.getResponseHeader('X-Link-Previous'),
+                  next: request.getResponseHeader('X-Link-Next')
+                }
+                var title = {
+                  previous: request.getResponseHeader('X-Title-Previous'),
+                  next: request.getResponseHeader('X-Title-Next')
+                }
                 // Canonical link
-                request.getResponseHeader('X-Link-Canonical');
-                // Index link
+                $('link[rel="canonical"]').attr('href', link.canonical);
                 // Previous link
+                $('link[rel="prev"]').attr('href', link.previous);
+                $('link[rel="prev"]').attr('title', title.previous);
+                if ($('link[rel="prev"]').length === 0) {
+                  $('head').append('<link rel="previous" title="' + title.previous + '" href="' + link.previous + '" />');
+                }
                 // Next link
+                $('link[rel="next"]').attr('href', link.next);
+                $('link[rel="next"]').attr('title', title.next);
+                if ($('link[rel="next"]').length === 0) {
+                  $('head').append('<link rel="previous" title="' + title.next + '" href="' + link.next + '" />');
+                }
             });
         });
     });
